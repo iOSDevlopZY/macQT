2b1c7bd3c93f838bb47bf9d994535130a57352da
iOSDevlopZY <qdzzc@163.com>
2020-10-27 15:19:47 +0800

    commit

--- DragDropDemo/draglabel.cpp
+++ DragDropDemo/draglabel.cpp
@@ -34,7 +34,7 @@ void DragLabel::mousePressEvent(QMouseEvent *e)
     //2 自定义Mime类型数据，存储图片数据和位置信息
     QByteArray itemData;
     QDataStream dataStream(&itemData,QIODevice::WriteOnly);
-    dataStream<<pixmap;
+    dataStream << pixmap;
 
     //3 将数据字节数组存入MimeData中，这里相当于setData作用类似<key,value>
     QMimeData *mimeData = new QMimeData;
